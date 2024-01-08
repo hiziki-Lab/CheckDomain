756735fc1192991bd575158a91b18cad0c598761
ひーくん <98318542+Hlikun@users.noreply.github.com>
2024-01-09 05:53:51 +0900

    コメントを追加

--- src/main/java/network/hiziki/checkdomain/event/events/Login.java
+++ src/main/java/network/hiziki/checkdomain/event/events/Login.java
@@ -1,6 +1,5 @@
 package network.hiziki.checkdomain.event.events;
 
-
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.LoginEvent;
@@ -17,16 +16,21 @@ public class Login {
         InetSocketAddress virtualHost = connection.getVirtualHost();
         String hostName = virtualHost.getHostName();
 
-        if (!Main.getDomainList().contains(hostName)) { // 許可されるドメインか確認
-            String action = Main.getUnmatchedDomainAction(); // ドメインが一致しない場合の処理を取得
+        // 許可されるドメインか確認
+        if (!Main.getDomainList().contains(hostName)) {
+            // ドメインが一致しない場合の処理を取得
+            String action = Main.getUnmatchedDomainAction();
 
+            // 許可する場合はreturn
             if (action.equals("allow")) return;
 
             String kickMessage = String.join("\n", Main.getKickMessage());
 
+            // 接続を禁止
             e.setCancelled(true);
             e.setCancelReason(new TextComponent(kickMessage));
 
+            // 「kickLogging」の場合はログに記録
             if (action.equals("kickLogging")) {
                 Main.getPlugin().getLogger().info(name + "が許可されていないドメイン「" + hostName + "」で接続を行いました。");
             }
